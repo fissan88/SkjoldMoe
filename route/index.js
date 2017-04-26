1680592e9bc1659e22c449239ac2351e545122d3
Jeppe Lindstrøm Jensen <jeppelindstromjensen@gmail.com>
2017-04-26 11:15:36 +0200

    Fixed templating completely!

--- route/index.js
+++ route/index.js
@@ -12,8 +12,9 @@ arrVarer =  [
 module.exports = function (express) {
     var router = express.Router();
 
-    router.route('/')
+    router.route('/');
         .get(function (req, res) {
+            // res.send("Herro");
             res.render('layout', {
                 vareListe: arrVarer
             });
