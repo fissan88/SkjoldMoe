45fa17f9b3370d5605185dd17664c405945b43d3
Jeppe Lindstrøm Jensen <jeppelindstromjensen@gmail.com>
2017-04-26 12:10:32 +0200

    Fixed templating completely completely!

--- route/index.js
+++ route/index.js
@@ -1,23 +1,24 @@
 /**
  * Created by eaajejen54 on 20-04-2017.
  */
-arrVarer =  [
-    {vareNavn: "vare1", udløbsDato: "i morgen", antal: "10"},
-    {vareNavn: "vare2", udløbsDato: "i morgen", antal: "10"},
-    {vareNavn: "vare3", udløbsDato: "i morgen", antal: "10"},
-    {vareNavn: "vare4", udløbsDato: "i morgen", antal: "10"},
-    {vareNavn: "vare5", udløbsDato: "i morgen", antal: "10"}
-];
 
 module.exports = function (express) {
     var router = express.Router();
 
+    var arrVarer =  [
+        {vareNavn: "vare1", udløbsDato: "i morgen", antal: "10"},
+        {vareNavn: "vare2", udløbsDato: "i morgen", antal: "10"},
+        {vareNavn: "vare3", udløbsDato: "i morgen", antal: "10"},
+        {vareNavn: "vare4", udløbsDato: "i morgen", antal: "10"},
+        {vareNavn: "vare5", udløbsDato: "i morgen", antal: "10"}
+    ];
+
     router.route('/')
         .get(function (req, res) {
-            // res.send("Herro");
-            res.render('layout', {
+            res.render('index', {
                 vareListe: arrVarer
             });
         });
+    
     return router;
 };
