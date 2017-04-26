35ab17d1bcc47560616c8c6a6dfba7fb508b4532
Jeppe Lindstrøm Jensen <jeppelindstromjensen@gmail.com>
2017-04-26 12:08:09 +0200

    Fixed templating completely completely!

--- public/script.js
+++ public/script.js
@@ -3,4 +3,31 @@
  */
 $(document).ready(function() {
 
+    function compileNewBody(templateName) {
+        $.get('../views/' + templateName, function (template) {
+            $('#container').empty();
+            var compiled = Handlebars.compile(template);
+            var html = compiled({
+
+            });
+            $('#container').append(html);
+        });
+    }
+
+    $('#btnDatoliste').click(function() {
+        compileNewBody("index.hbs");
+    });
+
+    $('#btnRegistrer').click(function() {
+         compileNewBody("registrer.hbs");
+    });
+
+    $('#btnKasseret').click(function() {
+        compileNewBody("kasseret.hbs");
+    });
+
+    $('#btnStatistik').click(function() {
+        compileNewBody("statistik.hbs");
+    });
+
 });
