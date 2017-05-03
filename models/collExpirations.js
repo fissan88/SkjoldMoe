d0d09c92e679d06b02e9588af3ec39254d894289
Kes Lagoni Williams <kesw34@gmail.com>
2017-05-03 11:01:12 +0200

    schema test

--- models/collExpirations.js
+++ models/collExpirations.js
@@ -1,3 +1,12 @@
 /**
  * Created by Kes Williams on 03-05-2017.
  */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var collExpirations = new Schema({
+    productName: String,
+    date: Date,
+    quantity: Number
+},  {collection: 'collExpirations', versionKey: false});
