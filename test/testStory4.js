b8f3e21e74294bd1325a71b7020054929b4eea69
Steen Tingager Kristensen <steentingager@gmail.com>
2017-05-05 10:03:08 +0200

    Det er lavet flere ting!

--- test/testStory4.js
+++ test/testStory4.js
@@ -8,8 +8,8 @@ mongoose.connect("mongodb://user:1234@ds111461.mlab.com:11461/skjoldmoe").connec
 describe('Database Manipulation Krav', () => {
 
     var testDate = new Date;
-    var testItem = [];
-    
+    var testItem;
+
     describe("Registeringen virker hvis...", () => {
         it("Opret og save dato objekt virker", (done) => {
             var testObject = new collExp({
@@ -23,23 +23,24 @@ describe('Database Manipulation Krav', () => {
         it("Objektet er blevet hentet og verificeret fra databasen", (done) => {
 
 
-            collExp.find('collExpirations', {barcode: "57045399", date: testDate.getDate(), quantity: 5}, (err, item) => {
+            collExp.findOne({barcode: "57045399", date: testDate.getDate(), quantity: 5}, (err, item) => {
                 if (err) {
                     done(err);
                 } else {
                     testItem = item;
-                    assert.equal(testItem[0].barcode, 57045399);
-                    var itemDate = new Date(testItem[0].date);
+                    assert.equal(testItem.barcode, 57045399);
+                    var itemDate = new Date(testItem.date);
                     assert.equal(itemDate.getDate(), testDate.getDate());
-                    assert.equal(testItem[0].quantity, 5);
+                    assert.equal(testItem.quantity, 5);
                     done();
                 }
             });
         });
 
         it("Objektet er blevet slettet igen fra databasen", (done) => {
+            var id = new mongoose.Types.ObjectId(testItem["_id"]);
 
-            collExp.findOneAndRemove('collExpirations',{_id: testItem[0]["_id"]}, done())
+            collExp.deleteOne('collExpirations',{_id: id}, done())
         });
     })
 });
