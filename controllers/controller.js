4d9ab36662c88332e4085c6c179ca9975c410be0
Kasper Knudsen <kasper.j.k@gmail.com>
2017-05-05 12:01:45 +0200

    tilføjet controller

---
+++ controllers/controller.js
@@ -0,0 +1,17 @@
+/**
+ * Created by kaspe on 05-05-2017.
+ */
+
+
+
+function createCollExpiration(barcode, date, quantity){
+
+}
+
+function getCollExpiration(barcode, date, quantity){
+
+}
+
+function deleteCollExpiration(barcode, date, quantity) {
+
+}
