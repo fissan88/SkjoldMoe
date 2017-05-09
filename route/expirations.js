84658b100e1afd355ecbbbc2a2bee08471c3dde8
Patrick W. Meyer <tbt_paddik@hotmail.com>
2017-05-09 09:04:05 +0200

    Her er der en opdtering før at vi skal lave routes cha cha cha

---
+++ route/expirations.js
@@ -0,0 +1,11 @@
+/**
+ * Created by PWM on 09-05-2017.
+ */
+
+const controller = require('../controllers/controller');
+const collProduc = require('../models/product');
+
+module.exports = (express) => {
+    const router = express.Router();
+
+};
