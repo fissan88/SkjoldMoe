635d8d3ee2311283a0a66c772dfead097826f06c
Kes Lagoni Williams <kesw34@gmail.com>
2017-05-10 12:46:33 +0200

    scripts er klar til story 4

---
+++ public/scripts/story4Script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Kes Williams on 10-05-2017.
+ */
+var expList = [];
+
+var populateExpList = () => {
+    $.get('./api/collExpirations/')
+        .done(function (data) {
+            expList.empty;
+            forEach(data).push(expList);
+        })
+        .fail(console.log("populateExpList .get failed!"));
+};
+
+var registerExp = (barcode, date, quantity) => {
+    $.post('./api/collExpirations/', {'barcode' : barcode, 'date' : date, 'quantity' : quantity})
+        .done( function (data) {
+            console.log(data);
+        })
+};
+
+$(document).ready(function () {
+    populateExpList();
+
+});
