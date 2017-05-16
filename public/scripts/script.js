b65baa390dab9059652b2420c246c986cf875463
Patrick W. Meyer <tbt_paddik@hotmail.com>
2017-05-16 10:50:50 +0200

    Merge branch 'I2S1' of C:\Users\Eqliphex\Dropbox\Skole ting\EAAA\DIP\SkjoldMoe with conflicts.

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,94 +1,93 @@
 /**
  * Created by tuxzo on 03-04-2017.
  */
+$(document).ready(function() {
 // RENDERING OF PAGES
 // =============================================================================
-
-function compileNewBody(templateName) {
-    $.get('../views/' + templateName, function (template) {
-        $('#container').empty();
-        let compiled = Handlebars.compile(template);
-        let html = compiled({});
-        $('#container').append(html);
-    });
-}
-
-function populateSortimentList() {
-    $('#stockList').empty();
-
-    $.get('/api/products', (req, res) => {
-        for (let i in req) {
-            $('#stockList').append('<li class="list-group-item">'
-                + '<label>'+req[i]._id + ' - ' + req[i].name+'</label>'
-                + '<div class="pull-right action-buttons">'
-                + '<a href="#" data-barcode="' + req[i]._id + '"><span class="glyphicon glyphicon-pencil" id="glyph' + req[i]._id + '"></span></a>'
-                + '<a href="#" class="trash"><span class="glyphicon glyphicon-trash"></span></a>'
-                + '</div></li>');
-
-            $('#glyph' + req[i]._id).on('click', () => {
-                editGoods(req[i]._id, req[i].name, req[i].isDryGoods);
-            });
-        }
-    });
-}
-
-// FUNCTIONS GETTING FROM SERVER
-// =============================================================================
-
-function registerExp(barcode, date, quantity) {
-    return new Promise((resolve, reject) => {
-        $.post('./api/collExpirations/', {
-            'barcode': barcode,
-            'date': date,
-            'quantity': quantity
-        })
-            .done(function (data) {
-                    console.log(data);
-                    resolve();
-                }
-            ).fail(() => {
-            alert("Kunne ikke oprette datoregisteringen");
-            reject();
-        })
-    });
-}
-
-function editGoods(barcode, name, isDryGoods) {
-    $('#editGoodsModal').modal("show");
-    $('#nameModal').val(name);
-    $('#barcodeModal').val(barcode);
-
-    if (isDryGoods) {
-        $('#chbIsDryGoodsModal').prop('checked', true);
-    } else {
-        $('#chbIsDryGoodsModal').prop('checked', false);
+    function compileNewBody(templateName) {
+        $.get('../views/' + templateName, function (template) {
+            $('#container').empty();
+            var compiled = Handlebars.compile(template);
+            var html = compiled({});
+            $('#container').append(html);
+        });
     }
 
-    $('#btnOpdaterModal').click(() => {
-        if ($('#nameModal').val() != name || $('#chbIsDryGoodsModal').is(':checked') != isDryGoods) {
-            updateProduct($('#barcodeModal').val(), $('#nameModal').val(), $('#chbIsDryGoodsModal').is(':checked'));
-        }
-    })
-}
-
-function updateProduct(barcode, name, isDryGoods) {
-    let updatedProduct = {'_id': barcode, 'name': name, 'isDryGoods': isDryGoods};
-    $.ajax({
-        type: "PUT",
-        url: "/api/products/",
-        contentType: "application/json",
-        data: JSON.stringify(updatedProduct)
-    }).done(() => {
-        alert("Varen blev opdateret succesfuldt.");
-        populateSortimentList();
+    $('#btnDatoliste').click(function() {
+        compileNewBody("index.hbs");
+        addItemsToExpirationLists();
     });
-}
+
+    $('#btnRegistrer').click(function() {
+        renderRegistrer();
+    });
+
+    $('#btnKasseret').click(function() {
+        compileNewBody("kasseret.hbs");
+    });
+
+    $('#btnStatistik').click(function() {
+        compileNewBody("statistik.hbs");
+    });
+
+    function renderRegistrer(){
+        compileNewBody("registrer.hbs");
+        $.get('/api/products', (req, res) =>{
+            for(let i in req){
+                $('#wareList').append('<li  id="listItem">' + '<a href="#" data-barcode="'+req[i]._id+'">' + req[i]._id + ' - ' + req[i].name +'' + '</a>' + '</li>');
+            }
+        });
+    };
+
+// BUTTON ACTIONS
+// =============================================================================
+    $(document).on('click','#btnCreateProduct', function() {
+        console.log("I'm alive");
+
+        var newProduct = {
+            _id : $('#newProductBarcode').val(),
+            name : $('#newProductName').val(),
+            isDryGoods : $('#isDryGoods').is(':checked')
+        };
+
+        $.post("/api/products", newProduct, function(data) {
+            alert(data);
+        });
+
+        renderRegistrer();
+    });
+
+    $(document).on('click', 'li',(event)=>{
+        console.log($(event.target).data('barcode'));
+        $('#selectedProductBarcode').val($(event.target).data('barcode'));
+
+    });
+
+    // Markerer en række på vores liste
+    $(document).on('click', 'table tr', (event)=> {
+        $('tr.active').removeClass('active');
+        $(event.target.parentNode).addClass('active');
+        console.log($(event.target.parentNode).data('id'));
+
+    });
+
+    // TODO skal have opdateret vores varer på listen med en ny glyphicon og antal
+    // gemmer det nye antal af varer
+    $(document).on('click', '#saveBtn', (event)=> {
+        alert("CLICKED");
+        // let numberOfGoodsOnDiscount = $('#numberInput').val();
+        //
+        // $.put("/api/collExpirations/" + id, newProduct, function(data) {
+        //     alert(data);
+        // });
+    });
+});
 
 function addItemsToExpirationLists(){
     $.get('/api/collExpirations', (req, res) =>{
         for(let i in req){
-            var currentDate = new Date;
-            var month = currentDate.getMonth();
+            let currentDate = new Date;
+            let month = currentDate.getMonth();
             month = monthConverter(month);
             var currentDateString = currentDate.getFullYear() + '-' + month + '-' + currentDate.getDate();
 
@@ -114,89 +113,4 @@ function addItemsToExpirationLists(){
         }
         return month
     }
-
-$(document).ready(function () {
-
-// BUTTON ACTIONS
-// =============================================================================
-
-    function toggleButtons() {
-        $('#btnDatoliste').removeClass('active');
-        $('#btnRegistrer').removeClass('active');
-        $('#btnSortiment').removeClass('active');
-    }
-
-    $('#btnDatoliste').click(function () {
-        compileNewBody("index.hbs");
-        toggleButtons();
-        $('#btnDatoliste').addClass('active');
-    });
-
-    $('#btnRegistrer').click(function () {
-        compileNewBody("registrer.hbs");
-        populateSortimentList();
-        toggleButtons();
-        $('#btnRegistrer').addClass('active');
-    });
-
-    $('#btnSortiment').click(function () {
-        compileNewBody("sortiment.hbs");
-        populateSortimentList();
-        toggleButtons();
-        $('#btnSortiment').addClass('active');
-    });
-
-    // Markerer en række på vores liste
-    $(document).on('click', 'table tr', (event)=> {
-        $('tr.active').removeClass('active');
-        $(event.target.parentNode).addClass('active');
-        console.log($(event.target.parentNode).data('id'));
-
-    });
-
-    // TODO skal have opdateret vores varer på listen med en ny glyphicon og antal
-    // gemmer det nye antal af varer
-    $(document).on('click', '#saveBtn', (event)=> {
-        alert("CLICKED");
-        // let numberOfGoodsOnDiscount = $('#numberInput').val();
-        //
-        // $.put("/api/collExpirations/" + id, newProduct, function(data) {
-        //     alert(data);
-        // });
-    });
-});
-
-    $(document).on('click', '#btnCreateProduct', function () {
-
-        let newProduct = {
-            _id: $('#newProductBarcode').val(),
-            name: $('#newProductName').val(),
-            isDryGoods: $('#isDryGoods').is(':checked')
-        };
-
-        $.post("/api/products", newProduct, function (data) {
-            alert("Varen blev oprettet succesfuldt!");
-        }).done(() => {
-            $('#newProductBarcode').val("");
-            $('#newProductName').val("");
-            $('#isDryGoods').prop('checked', false);
-            populateSortimentList();
-        });
-    });
-
-    $(document).on('click', 'li', (event) => {
-        $('#selectedProductBarcode').val($(event.target).data('barcode'));
-    });
-
-    $(document).on('click', '#btnCreateExpiration', function () {
-        let barcode = $('#selectedProductBarcode').val();
-        console.log("testStory4 script: selected barcode val: " + barcode);
-        let date = $('#expDate').val();
-        console.log("testStory4 script: selected barcode val: " + date);
-
-        registerExp(barcode, date, 0).then(() => {
-            $('#selectedProductBarcode').val('');
-            $('#expDate').val('');
-        });
-    })
-});
+}
